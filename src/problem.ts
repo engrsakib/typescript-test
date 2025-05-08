d7c1f00a94a883362df8f9211fba6b1ba7fad2f2
engrsakib <snazmussakib01@gmail.com>
2025-05-08 17:28:18 +0600

    readme update

--- src/problem.ts
+++ src/problem.ts
@@ -5,7 +5,9 @@ function formatString(input: string, toUpper?: boolean): string {
   return input.toLocaleLowerCase();
 }
 
-console.log(formatString("sakib", false));
+
+
+
 
 function filterByRating(
   items: { title: string; rating: number }[]
@@ -13,25 +15,14 @@ function filterByRating(
   return items.filter((item) => item.rating >= 4);
 }
 
-const items = [
-  { title: "Item A", rating: 3.5 },
-  { title: "Item B", rating: 4.2 },
-  { title: "Item C", rating: 4.0 },
-  { title: "Item D", rating: 3.9 },
-  { title: "sakib", rating: 4.9 },
-];
-
-console.log(filterByRating(items));
 
 
-  function concatenateArrays<T>(...arrays: T[][]): T[] {
-    const result: T[] = [];
-    arrays.forEach(arr => result.push(...arr));
-    return result;
-  }
 
-  console.log(concatenateArrays(["a", "b"],["c"],["d"], ["sakib"]));
-  console.log(concatenateArrays([0, 4, 24],[41],[5, 8], [4]));
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  const result: T[] = [];
+  arrays.forEach((arr) => result.push(...arr));
+  return result;
+}
 
 
 
@@ -63,10 +54,6 @@ class Car extends Vehicle {
   }
 }
 
-const myCar = new Car("Honda", 2024, "Dream 110");
-console.log(myCar.getInfo());
-console.log(myCar.getModel());
-
 
 
 
@@ -77,76 +64,57 @@ function processValue(value: string | number): number {
   return (value as string).length;
 }
 
-console.log(processValue("programming-hero"));
-console.log(processValue(10));
+
+
 
 
 interface Product {
-    name: string;
-    price: number;
-  }
-  function getMostExpensiveProduct(products: Product[]): Product | null {
-    if (products.length === 0) return null;
-  
-    return products.reduce((maxSoFar, current) =>
-      current.price > maxSoFar.price ? current : maxSoFar
-    );
-  }
-  
-  const products: Product[] = [
-    { name: "Pen", price: 10 },
-    { name: "Notebook", price: 25 },
-    { name: "Bag", price: 50 }
-  ];
-  console.log(getMostExpensiveProduct(products));  
+  name: string;
+  price: number;
+}
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0) return null;
+
+  return products.reduce((maxSoFar, current) =>
+    current.price > maxSoFar.price ? current : maxSoFar
+  );
+}
 
 
 
-  enum Day {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
+
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  switch (day) {
+    case Day.Sunday:
+    case Day.Saturday:
+      return "Weekend";
+    default:
+      return "Weekday";
   }
-  
-  function getDayType(day: Day): string {
-    switch (day) {
-      case Day.Sunday:
-      case Day.Saturday:
-        return "Weekend";
-      default:
-        return "Weekday";
+}
+
+
+
+
+
+async function squareAsync(n: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+    if (n < 0) {
+      return reject(new Error("Negative number not allowed"));
     }
-  }
-  
-  
-  console.log(getDayType(Day.Monday));    
-  console.log(getDayType(Day.Wednesday));    
-  console.log(getDayType(Day.Friday));    
-  console.log(getDayType(Day.Saturday));  
-  console.log(getDayType(Day.Sunday)); 
-
-
-
-  async function squareAsync(n: number): Promise<number> {
-    return new Promise((resolve, reject) => {
-      if (n < 0) {
-        return reject(new Error("Negative number not allowed"));
-      }
-      setTimeout(() => {
-        resolve(n * n);
-      }, 1000);
-    });
-  }
-  
-  
-  squareAsync(4)
-    .then(result => console.log(result))  
-    .catch(err => console.error(err));
-  
-  squareAsync(-3)
-    .then(result => console.log(result))
-    .catch(err => console.error(err));     
+    setTimeout(() => {
+      resolve(n * n);
+    }, 1000);
+  });
+}
